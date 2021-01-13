edf24b3824d376abf503b73b26f535d17619fe7a
rosaamalia <amaliarosaaa15@gmail.com>
2021-01-13 17:03:02 +0700

    Revert "menambahkan fp lbe mis"
    This reverts commit 62dd8deb7604d3e348d91fdb8e3e9dcbf89e82a5.

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View } from 'react-native';
 
-import Home from './app/screen/Home';
-import NewestList from './app/screen/NewList';
-import Detail from './app/screen/Detail';
-
-const Stack = createStackNavigator();
-
-export default function App(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-                <Stack.Screen name="NewestList" component={NewestList} options={{headerShown: false}}/>
-                <Stack.Screen name="Detail" component={Detail} options={{headerShown: false}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
